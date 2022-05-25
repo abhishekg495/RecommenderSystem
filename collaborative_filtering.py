b53d3c9f83cee343104508f7d121245bb65be0e9
abhishekg495 <abhishekgupta23810306@gmail.com>
2022-05-26 04:26:24 +0530

    fixed common movie titles bug in collaborative filtering

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -11,33 +11,44 @@ class collaborative_filter:
         self.movies = pd.read_csv("Datasets/movies.csv")
         self.ratings = pd.read_csv("Datasets/ratings.csv")
         self.links = links.set_index("movieId")["imdb_link"]
-        self.id = self.movies[["movieId", "title"]].set_index("title")
+        self.movieId = self.movies[["movieId", "title"]].set_index(
+            "title"
+        )  ## GET MOVIE ID USING TITLE
+        self.movieTitle = self.movies[["movieId", "title"]].set_index(
+            "movieId"
+        )  ## GET MOVIE TITLE USING ID
 
+        ## PREPROCESSING DATA TO SET UP A CORRELATION MATRIX BETWEEN MOVIES ##########
         self.ratings = pd.merge(self.movies, self.ratings).drop(
             ["genres", "timestamp"], axis=1
         )
 
         self.movies_ratings = self.ratings.pivot_table(
-            index=["userId"], columns=["title"], values="rating"
+            index=["userId"], columns=["movieId"], values="rating"
         )
         self.movies_ratings = self.movies_ratings.dropna(thresh=10, axis=1).fillna(
             0, axis=1
         )
         self.movies_ratings.fillna(0, axis=1, inplace=True)
         self.corr_matrix = self.movies_ratings.corr(method="pearson")
+        #############################################################################
 
     def get_movies_list(self):
-        return self.corr_matrix.index
+        return self.movieTitle.loc[list(self.corr_matrix.index)]["title"]
 
     def recommend(self, user_ratings):
         if len(user_ratings) == 0:
             return []
-        movies_list = []
-        ratings_list = []
+        movies_list = []  ## STORE IDs OF MOVIES SELECTED BY USER
+        ratings_list = []  ## STORE RATINGS GIVEN BY USER
         for movie, rating in user_ratings:
-            movies_list.append(movie)
+            movies_list.append(self.movieId.loc[movie]["movieId"])
             ratings_list.append(rating)
-        similar_movies = self.corr_matrix[movies_list]
+        similar_movies = self.corr_matrix[
+            movies_list
+        ]  ## GET CORRELATION MATRIX FOR SELECTED MOVIES
+
+        ## ADJUST CORRELATION ACCORDING TO USER RATING AND SORT BY CORRELATION #######
         for i in range(len(ratings_list)):
             similar_movies.iloc[
                 :, similar_movies.columns.get_loc(movies_list[i])
@@ -45,15 +56,22 @@ class collaborative_filter:
         similar_movies = pd.DataFrame(
             similar_movies.sum(axis=1).sort_values(ascending=False)
         )
-        similar_movies["movieId"] = [
-            self.id.loc[movie_name]["movieId"] for movie_name in similar_movies.index
+        ###############################################################################
+
+        ### ADD A COLUMN FOR TITLES IN SORTED LIST (CONTAINING MOVIE IDs) #############
+        similar_movies["title"] = [
+            self.movieTitle.loc[movie_id]["title"]
+            for movie_id in list(similar_movies.index)
         ]
-        # similar_movies.sum().sort_values(ascending=False).head(20)
+        ##############################################################################
+
+        ######## ATTACH LINKS AND RETURN #############################################
         return pd.concat(
             [
-                similar_movies.head(30).reset_index().set_index("movieId")["title"],
+                similar_movies.head(30)["title"],
                 self.links,
             ],
             axis=1,
             join="inner",
         )
+        #############################################################################
